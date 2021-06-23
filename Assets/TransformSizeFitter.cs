dbe325c0f4c84a9eca812e777825dc36aa2da95f
daredevil2309 <daredevil2309@gmail.com>
2021-06-23 23:57:19 +0500

    Проба подгонки под размер родителя с помощью скрипта

---
+++ Assets/TransformSizeFitter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TransformSizeFitter : ContentSizeFitter
+{
+	[SerializeField] public RectTransform fitRect;
+
+    private RectTransform rect;
+    private RectTransform Rect
+	{
+        get
+		{
+            if (rect == null)
+            {
+                rect = GetComponent<RectTransform>();
+            }
+
+            return rect;
+		}
+	}
+
+	public override void SetLayoutHorizontal()
+	{
+		base.SetLayoutHorizontal();
+		FitInParent();
+	}
+
+	public override void SetLayoutVertical()
+	{
+		base.SetLayoutVertical();
+		FitInParent();
+	}
+
+
+
+	private void FitInParent()
+	{
+		if (transform.parent == null)
+		{
+			return;
+		}
+
+		float rectWidth = Rect.rect.width;
+		float rectHeight = Rect.rect.height;
+		float parentWidth = fitRect.rect.width;
+		float parentHeight = fitRect.rect.height;
+
+		float width = Mathf.Clamp(rectWidth, 0, parentWidth);
+		float height = Mathf.Clamp(rectHeight, 0, parentHeight);
+
+		Rect.sizeDelta = new Vector2(width, height);
+	}
+}
---
+++ Assets/TransformSizeFitterEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+
+[CustomEditor(typeof(TransformSizeFitter))]
+[CanEditMultipleObjects]
+public class TransformSizeFitterEditor : ContentSizeFitterEditor
+{
+	SerializedProperty fitRect;
+
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+
+		fitRect = serializedObject.FindProperty("fitRect");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		EditorGUILayout.PropertyField(fitRect, new GUIContent("Fit Rect"));
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
